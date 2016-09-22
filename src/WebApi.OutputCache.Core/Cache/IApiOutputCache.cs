4f2318b16a27275361afe90d5667322c8fd5a606
Jon Sagara <jon@sagarasoftware.com>
2016-09-22 10:56:43 -0700

    Added async methods to the IApiOutputCache interface.

--- src/WebApi.OutputCache.Core/Cache/IApiOutputCache.cs
+++ src/WebApi.OutputCache.Core/Cache/IApiOutputCache.cs
@@ -1,22 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace WebApi.OutputCache.Core.Cache
 {
     public interface IApiOutputCache
     {
         void RemoveStartsWith(string key);
+        Task RemoveStartsWithAsync(string key);
 
         T Get<T>(string key) where T : class;
+        Task<T> GetAsync<T>(string key) where T : class;
 
         [Obsolete("Use Get<T> instead")]
         object Get(string key);
+        [Obsolete("Use GetAsync<T> instead")]
+        Task<object> GetAsync(string key);
 
         void Remove(string key);
 
         bool Contains(string key);
+        Task<bool> ContainsAsync(string key);
 
         void Add(string key, object o, DateTimeOffset expiration, string dependsOnKey = null);
+        Task AddAsync(string key, object o, DateTimeOffset expiration, string dependsOnKey = null);
 
         IEnumerable<string> AllKeys { get; }
     }
--- src/WebApi.OutputCache.Core/Cache/MemoryCacheDefault.cs
+++ src/WebApi.OutputCache.Core/Cache/MemoryCacheDefault.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Caching;
+using System.Threading.Tasks;
 
 namespace WebApi.OutputCache.Core.Cache
 {
+    /// <summary>
+    /// In-memory cache. Does not support async operations. Calling them will result in a NotSupportedException.
+    /// </summary>
     public class MemoryCacheDefault : IApiOutputCache
     {
         private static readonly MemoryCache Cache = MemoryCache.Default;
@@ -17,16 +21,38 @@ namespace WebApi.OutputCache.Core.Cache
             }
         }
 
+        public Task RemoveStartsWithAsync(string key)
+        {
+            throw new NotSupportedException();
+        }
+
+        public virtual void RemoveStartsWith(string baseKey, string key)
+        {
+            // Doesn't make sense with the MemoryCacheDefault implementation. Just use the default behavior
+            //   of using dependent keys and cache invalidation callbacks.
+            throw new NotImplementedException("For MemoryCacheDefault, use RemoveStartsWith(string)");
+        }
+
         public virtual T Get<T>(string key) where T : class
         {
             var o = Cache.Get(key) as T;
             return o;
         }
 
+        public Task<T> GetAsync<T>(string key) where T : class
+        {
+            throw new NotSupportedException();
+        }
+
         [Obsolete("Use Get<T> instead")]
         public virtual object Get(string key)
         {
             return Cache.Get(key);
+        }
+
+        public Task<object> GetAsync(string key)
+        {
+            throw new NotSupportedException();
         }
 
         public virtual void Remove(string key)
@@ -40,6 +66,16 @@ namespace WebApi.OutputCache.Core.Cache
         public virtual bool Contains(string key)
         {
             return Cache.Contains(key);
+        }
+
+        public Task<bool> ContainsAsync(string key)
+        {
+            throw new NotSupportedException();
+        }
+
+        public Task<bool> ContainsAsync(string baseKey, string key)
+        {
+            throw new NotSupportedException();
         }
 
         public virtual void Add(string key, object o, DateTimeOffset expiration, string dependsOnKey = null)
@@ -60,6 +96,11 @@ namespace WebApi.OutputCache.Core.Cache
                 Cache.Add(key, o, cachePolicy);
             }
         }
+
+        public Task AddAsync(string key, object o, DateTimeOffset expiration, string dependsOnKey = null)
+        {
+            throw new NotSupportedException();
+        }        
 
         public virtual IEnumerable<string> AllKeys
         {
