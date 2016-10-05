c1e1b5ae0f07ccb2b82eb34134db526ef33df5e9
Jon Sagara <jon@sagarasoftware.com>
2016-10-05 10:45:40 -0700

    Published 1.0.7-beta.

--- NuGetPublish.bat
+++ NuGetPublish.bat
@@ -1,3 +1,3 @@
-nuget push WebApi2.RedisOutputCache.1.0.6-beta.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push WebApi2.RedisOutputCache.1.0.7-beta.nupkg -Source https://www.nuget.org/api/v2/package
 
 PAUSE
