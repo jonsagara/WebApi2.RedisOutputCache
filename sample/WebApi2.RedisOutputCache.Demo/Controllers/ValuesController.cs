3c0a8c824f7bc36d23b0c0535d9cee7e19f8b24d
Jon Sagara <jon@sagarasoftware.com>
2016-10-05 22:13:59 -0700

    Hooked up SE.Redis to Autofac for DI.

--- sample/WebApi2.RedisOutputCache.Demo/Controllers/ValuesController.cs
+++ sample/WebApi2.RedisOutputCache.Demo/Controllers/ValuesController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
+using StackExchange.Redis;
 
 namespace WebApi2.RedisOutputCache.Demo.Controllers
 {
--- sample/WebApi2.RedisOutputCache.Demo/Global.asax.cs
+++ sample/WebApi2.RedisOutputCache.Demo/Global.asax.cs
@@ -2,6 +2,8 @@
 using System.Web.Http;
 using Autofac;
 using Autofac.Integration.WebApi;
+using StackExchange.Redis;
+using WebApi2.RedisOutputCache.Demo.Caching;
 
 namespace WebApi2.RedisOutputCache.Demo
 {
@@ -16,6 +18,10 @@ namespace WebApi2.RedisOutputCache.Demo
 
             // Register Web API controllers.
             builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // Register other services.
+            builder.Register<IDatabase>(ctx => RedisCache.Multiplexer.GetDatabase());
+            builder.Register<ISubscriber>(ctx => RedisCache.Multiplexer.GetSubscriber());
 
             // Set the dependency resolver to be Autofac.
             var container = builder.Build();
