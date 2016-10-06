34977a5f21d8c54f2f5056ebad590905655fdba2
Jon Sagara <jon@sagarasoftware.com>
2016-10-05 21:49:44 -0700

    Added NLog and SE.Redis.

---
+++ sample/WebApi2.RedisOutputCache.Demo/Caching/RedisCache.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using NLog;
+using StackExchange.Redis;
+
+namespace WebApi2.RedisOutputCache.Demo.Caching
+{
+    public static class RedisCache
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static readonly ConnectionMultiplexer Multiplexer;
+
+        static RedisCache()
+        {
+            Multiplexer = ConnectionMultiplexer.Connect(ConfigurationManager.ConnectionStrings["RedisCache"].ConnectionString);
+
+            Multiplexer.ConnectionFailed += (o, e) =>
+            {
+                Logger.Error(e.Exception, $"ConnectionFailed: ConnectionType = {e.ConnectionType} reported FailureType = {e.FailureType} on EndPoint = {e.EndPoint}.");
+            };
+
+            Multiplexer.ConnectionRestored += (o, e) =>
+            {
+                Logger.Info($"ConnectionRestored: ConnectionType = {e.ConnectionType} reported FailureType = {e.FailureType} on EndPoint = {e.EndPoint}.");
+            };
+        }
+    }
+}
