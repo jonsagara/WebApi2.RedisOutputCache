36024c46cff11c72edbf5b1e9fe83c9ac448f2a8
Jon Sagara <jon@sagarasoftware.com>
2016-10-09 21:20:30 -0700

    First POC end-to-end test using WebApi2's HttpServer for in-memory hosting.

---
+++ sample/WebApi2.RedisOutputCache.Demo.EndToEndTests/HostFixture.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using StackExchange.Redis;
+using WebApi2.RedisOutputCache.Core.Caching;
+using WebApi2.RedisOutputCache.Demo.Caching;
+
+namespace WebApi2.RedisOutputCache.Demo.EndToEndTests
+{
+    public class HostFixture
+    {
+        private static readonly HttpServer _server;
+        private static readonly HttpClient _client;
+
+        static HostFixture()
+        {
+            var config = new HttpConfiguration();
+
+            // Initial configuration.
+            WebApiConfig.Register(config);
+
+            // Autofac
+            var builder = new ContainerBuilder();
+
+            // Register Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+
+            //
+            // Register services to support redis output caching.
+            //
+
+            // StackExchange.Redis
+            builder.Register<IDatabase>(ctx => RedisCache.Multiplexer.GetDatabase());
+            builder.Register<ISubscriber>(ctx => RedisCache.Multiplexer.GetSubscriber());
+
+            // Output caching
+            builder.RegisterType<RedisApiOutputCache>().As<IApiOutputCache>();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            _server = new HttpServer(config);
+            _client = new HttpClient(_server);
+        }
+
+        public HttpServer Server => _server;
+        public HttpClient Client => _client;
+    }
+}
---
+++ sample/WebApi2.RedisOutputCache.Demo.EndToEndTests/Values/ValuesTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebApi2.RedisOutputCache.Demo.EndToEndTests.Values
+{
+    public class ValuesTests : IClassFixture<HostFixture>
+    {
+        private readonly HostFixture _hostFixture;
+
+        public ValuesTests(HostFixture hostFixture)
+        {
+            _hostFixture = hostFixture;
+        }
+
+
+        [Fact]
+        public async Task Test()
+        {
+            var result = await _hostFixture.Client.GetAsync("http://localhost/api/values");
+
+            Assert.True(result.IsSuccessStatusCode);
+            var resultString = await result.Content.ReadAsStringAsync();
+        }
+    }
+}
