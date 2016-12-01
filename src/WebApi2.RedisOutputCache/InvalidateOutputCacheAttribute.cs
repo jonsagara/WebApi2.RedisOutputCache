a6fedecd5c4e7bd0cf17bcc2b99aa8c5ee647b63
Jon Sagara <jon@sagarasoftware.com>
2016-12-01 11:37:48 -0800

    The strong named version should depend on StackExchange.Redis.StrongName.

--- src/WebApi2.RedisOutputCache/InvalidateOutputCacheAttribute.cs
+++ src/WebApi2.RedisOutputCache/InvalidateOutputCacheAttribute.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Filters;
 using NLog;
+using WebApi2.RedisOutputCache;
 using WebApi2.RedisOutputCache.Extensions;
 
 namespace WebApi2.RedisOutputCache
