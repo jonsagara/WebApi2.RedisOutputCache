1f799c787c6f917dd95e0154e80f73f1bff46347
Jon Sagara <jon@sagarasoftware.com>
2016-12-27 10:15:15 -0800

    .NET 4.5 support. Version bump.

--- NuGetPublish.bat
+++ NuGetPublish.bat
@@ -1,3 +1,3 @@
-nuget push WebApi2.RedisOutputCache.1.0.9-beta.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push WebApi2.RedisOutputCache.1.0.15-beta.nupkg -Source https://www.nuget.org/api/v2/package
 
 PAUSE
