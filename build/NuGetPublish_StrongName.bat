abe9d91aadf60f46f497f3cde830c339be24ee7d
Jon Sagara <jon@sagarasoftware.com>
2017-01-31 13:33:33 -0800

    Upgraded NLog. Bumped the version number.

--- build/NuGetPublish_StrongName.bat
+++ build/NuGetPublish_StrongName.bat
@@ -1,3 +1,3 @@
-nuget push WebApi2.RedisOutputCache.StrongName.1.0.15-beta.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push WebApi2.RedisOutputCache.StrongName.1.0.17-beta.nupkg -Source https://www.nuget.org/api/v2/package
 
 PAUSE
